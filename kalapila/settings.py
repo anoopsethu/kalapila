6de43a4f6efd959e20e38a111a584bd17fba9796
Anoop Sethumadhavan <anoopsethu@Anoops-MacBook-Air.local>
2017-04-22 16:36:07 +0530

    More changes to settings.py file

--- kalapila/settings.py
+++ kalapila/settings.py
@@ -132,7 +132,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
